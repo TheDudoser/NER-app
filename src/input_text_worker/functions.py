7592c26f80aeba20b4bd78eddd113d0048b20ead
khmelevskiy_ed <khmelevskiy_ed@farpost.com>
2025-05-10 21:15:17 +1000

    #11: Небольшой рефакторинг

--- src/input_text_worker/functions.py
+++ src/input_text_worker/functions.py
@@ -1,12 +1,5 @@
 import json
-from pathlib import Path
 import hashlib
-
-
-def read_file_text_examples(file_name: str) -> str:
-    base_path = Path(__file__).absolute().parent.parent.parent / 'text_examples'
-    with open(str(base_path / file_name), "r", encoding="utf-8") as file:
-        return file.read()
 
 
 def get_json_hash(data):
--- src/printer/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cli_printer import print_report
-
-__all__ = ['print_report']
